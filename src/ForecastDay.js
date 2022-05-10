cea67bd3136f2bc6f461c38f60132923c45fde32
XENIAAPavlova <96259552+XENIAAPavlova@users.noreply.github.com>
2022-05-10 18:59:24 +0200

    Removed fahrenheit unit

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -21,50 +21,17 @@ export default function ForecastDay(props) {
     return days[day];
   }
 
-  console.log('debuggg forecast day')
+  console.log("debuggg forecast day");
   console.log(props.data);
 
   return (
-      <div className="">
-        <div className="forecast-day fw-bold mb-2">{day()}</div>
-        <Icon icon={props.data.weather[0].icon} />
-        <div className="forecast-temperature mt-2">
-          <span className="forecast-temperature-max">{maxTemperature()}</span>
-          <span className="forecast-temperature-min">{minTemperature()}</span>
-        </div>
+    <div className="">
+      <div className="forecast-day fw-bold mb-2">{day()}</div>
+      <Icon icon={props.data.weather[0].icon} />
+      <div className="forecast-temperature mt-2">
+        <span className="forecast-temperature-max">{maxTemperature()}</span>
+        <span className="forecast-temperature-min">{minTemperature()}</span>
       </div>
-      /* <div className="col-sm-2">
-        <div className="forecast-day fw-bold mb-2">{day()}</div>
-        <Icon icon={props.data.weather[0].icon} />{" "}
-        <div className="forecast-temperature mt-2">
-          <span className="forecast-temperature-max">{maxTemperature()}</span>
-          <span className="forecast-temperature-min">{minTemperature()}</span>
-        </div>
-      </div>
-      <div className="col-sm-2">
-        <div className="forecast-day fw-bold mb-2">{day()}</div>
-        <Icon icon={props.data.weather[0].icon} />{" "}
-        <div className="forecast-temperature mt-2">
-          <span className="forecast-temperature-max">{maxTemperature()}</span>
-          <span className="forecast-temperature-min">{minTemperature()}</span>
-        </div>{" "}
-      </div>
-      <div className="col-sm-2">
-        <div className="forecast-day fw-bold mb-2">{day()}</div>
-        <Icon icon={props.data.weather[0].icon} />{" "}
-        <div className="forecast-temperature mt-2">
-          <span className="forecast-temperature-max">{maxTemperature()}</span>
-          <span className="forecast-temperature-min">{minTemperature()}</span>
-        </div>{" "}
-      </div>
-      <div className="col-sm-2">
-        <div className="forecast-day fw-bold mb-2">{day()}</div>
-        <Icon icon={props.data.weather[0].icon} />{" "}
-        <div className="forecast-temperature mt-2">
-          <span className="forecast-temperature-max">{maxTemperature()}</span>
-          <span className="forecast-temperature-min">{minTemperature()}</span>
-        </div>{" "}
-      </div> */
-
+    </div>
   );
 }
--- src/ShowTemperature.js
+++ src/ShowTemperature.js
@@ -1,61 +1,61 @@
 import React, { useState } from "react";
 
 export default function ShowTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
+  // const [unit, setUnit] = useState("celsius");
 
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
+  // function showFahrenheit(event) {
+  //   event.preventDefault();
+  //   setUnit("fahrenheit");
+  // }
 
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
+  // function showCelsius(event) {
+  //   event.preventDefault();
+  //   setUnit("celsius");
+  // }
 
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
+  // function fahrenheit() {
+  //   return (props.celsius * 9) / 5 + 32;
+  // }
 
-  if (unit === "celsius") {
-    return (
-      <div className="ShowTemperature">
-        <span className="temperature">
-          <h3>
-            {Math.round(props.celsius)}
-            <span className="unit text-center fs-5 fw-normal">
-              °C|
-              <a
+  // if (unit === "celsius") {
+  return (
+    <div className="ShowTemperature">
+      <span className="temperature">
+        <div className="number-grade fs-2 fw-bold">
+          {Math.round(props.celsius)}
+          <span className="unit text-start fw-normal fw-small">
+            °C
+            {/* <a
                 style={{ textDecoration: "none" }}
                 href="/"
                 onClick={showFahrenheit}
               >
                 °F
-              </a>
-            </span>
-          </h3>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="ShowTemperature">
-        <span className="temperature">
-          <h3>
-            {Math.round(fahrenheit())}
-            <span className="unit text-center fs-5 fw-normal">
-              <a
-                style={{ textDecoration: "none" }}
-                href="/"
-                onClick={showCelsius}
-              >
-                °C
-              </a>
-              |°F
-            </span>
-          </h3>
-        </span>
-      </div>
-    );
-  }
+              </a> */}
+          </span>
+        </div>
+      </span>
+    </div>
+  );
+  // } else {
+  //   return (
+  //     <div className="ShowTemperature">
+  //       <span className="temperature">
+  //         <h3>
+  //           {Math.round(fahrenheit())}
+  //           <span className="unit text-center fs-5 fw-normal">
+  //             <a
+  //               style={{ textDecoration: "none" }}
+  //               href="/"
+  //               onClick={showCelsius}
+  //             >
+  //               °C
+  //             </a>
+  //             |°F
+  //           </span>
+  //         </h3>
+  //       </span>
+  //     </div>
+  //   );
+  // }
 }
